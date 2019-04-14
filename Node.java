4cc75006608716ae969cda28c84b37c376ee3221
mayifanCode <792380497@qq.com>
2019-04-14 16:22:27 +0800

    HFMcompression

---
+++ Node.java
@@ -0,0 +1,82 @@
+package com.myf.HFMcompression1223;
+
+
+
+public class Node<T> implements Comparable<Node<T>>{
+
+	private T data;
+	private int weight;
+	private Node<T> left;
+	private Node<T> right;
+	
+	public Node(T data,int weight)
+	{
+		this.data=data;
+		this.weight=weight;
+	}
+	
+	
+	/**
+	 * 获取节点数据
+	 */
+	public String toString()
+	{
+		return "data:"+data+"   "+"weight:"+weight;
+	}
+
+	/**
+	 * 节点权值比较方法
+	 * @param o
+	 * @return
+	 */
+	public int compareTo(Node<T> o) {
+       
+		if(this.weight>o.weight)
+			return 1;
+		else if(this.weight<o.weight)
+			return -1;
+		return 0;
+	}
+	
+	public void setData(T data)
+	{
+		this.data=data;
+	}
+	
+	public void setWeight(int weight)
+	{
+		this.weight=weight;
+	}
+	
+	public T getData()
+	{
+		return data;
+	}
+	
+	public int getWeight()
+	{
+		return weight;
+	}
+	
+	public void setLeft(Node<T> node)
+	{
+		this.left=node;
+	}
+	
+	public void setRight(Node<T> node)
+	{
+		this.right=node;
+	}
+	
+	public Node<T> getLeft()
+	{
+		return this.left;
+	}
+	
+	public Node<T> getRight()
+	{
+		return this.right;
+	}
+	
+	
+}
